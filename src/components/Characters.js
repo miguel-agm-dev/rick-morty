671eb3bec0f7c58a8a93088d69e8dbed8c29afe3
miguel-agm-dev <miguel.agm.dev@gmail.com>
2025-04-29 13:24:06 +0200

    Recorriendo datos de api

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,8 +1,17 @@
 
 export default function Characters(props) {
+    const {characters} = props;
   return (
-    <div>
-      <h1>Desde characters JS</h1>
+    <div className="characters">
+      <h1>Personajes</h1>
+      <span className="back-home">Volver a la home</span>
+      <div className="container-characters">
+        {characters.map((character, index)=>(
+            <div className="character-container" key={index}>
+                <p>{character.name}</p>
+            </div>
+        ))}
+      </div>
     </div>
   )
 }
