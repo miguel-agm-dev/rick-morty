187d65ee841980de11bd6df6aec40f907e9e6ac6
miguel-agm-dev <miguel.agm.dev@gmail.com>
2025-04-29 12:04:19 +0200

    Union api

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import imgRickMorty from './img/rick-morty.png';
 import './App.css';
 
 function App() {
+
+  const reqApi = async () => {
+    const api= await fetch('https://rickandmortyapi.com/api/character')
+
+    console.log(api);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1 className='title'>Rick & Morty</h1>
         <img src={imgRickMorty} alt='Rick & Morty' className='img-home' />
-        button
+        <button onClick={reqApi} className='btn-search'>Buscar Personajes</button>
       </header>
     </div>
   );
