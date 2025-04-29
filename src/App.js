c8327886c0f5178ee2888d20107045ba576cf7a3
miguel-agm-dev <miguel.agm.dev@gmail.com>
2025-04-29 11:54:39 +0200

    Comienzo app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import imgRickMorty from './img/rick-morty.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className='title'>Rick & Morty</h1>
+        <img src={imgRickMorty} alt='Rick & Morty' className='img-home' />
+        button
       </header>
     </div>
   );
